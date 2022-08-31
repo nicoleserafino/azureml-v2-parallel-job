4db219ffe0dbd4c60407d62ca1fb8843b768d869
Ali-ry <ali.rezaei.786110@gmail.com>
2022-08-31 14:18:06 +0000

    adding ci workflow

--- data-engineering/data-engineering.py
+++ data-engineering/data-engineering.py
@@ -1,25 +1,73 @@
 import argparse,os
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
 # data engineering
 def prepare_training_data(args):
     # reading data from raw data file
-    raw_data_df = pd.read_csv(args.raw_data_file)
+    raw_data_df = pd.read_csv(args.raw_data_file, nrows=10000)
     # TODO: data engineering here for what can be done accross all tenants in the source data...
+    raw_data_df.rename(columns = {'InvoiceCount':'TotalInvoiceCount', 'Value':'OriginalInvoiceAmount'}, inplace = True)
+    raw_data_df=raw_data_df.loc[raw_data_df["OriginalInvoiceAmount"]>0]
+    raw_data_df["Due_Paid_weekDelta"] = raw_data_df["WofY_Paid"]-raw_data_df["WofY_Due"]
+
+    #Drop columns that are not needed
+    raw_data_df = raw_data_df.drop(columns=["DofM_Paid","MofY_Paid","MofY_Paid","WofY_Paid","DofWInMonth_Paid",
+    "CurrentInstanceDofWInMonth_Paid", "isFirstInstanceDayOfWeekInMonth_Paid","isLastInstanceDayOfWeekInMonth_Paid",
+    "PaymentType", "DofW_Paid", "TopPredictedPayDate", "Predicted","InvoiceBandValue","TotalInvoices",
+    "Prediction_Accuracy", "Raised_Paid_Variance",  "Due_Paid_Variance","lastDueM_Paid_Variance","StatementTransactionType",
+    "Due_TopPredictedPayDate_Variance","PercentageOfPaymentsUnknown","DofWInMonth_Due","DofWInMonth_Raised"] )
+
+ 
+    #SORT DATA BASED ON PAID DATE
+    raw_data_df.sort_values(by=["PaidDate"],inplace=True)
+    raw_data_df= raw_data_df.drop_duplicates(subset=["ClientID","InvoiceID"],keep="last")
+    
+    #Drop Date to make a simple LR model possible
+    #raw_data_df=raw_data_df.drop(columns=["DueDate","PaidDate","RaisedDate"])
+
+
+
     with open(os.path.join(args.training_data_folder,'MLTable'),'w') as MLTable_metadata_file:
         # start to generate MLTable metadata file
-        MLTable_metadata_file.write("paths:\n")
-        for tenant_id in raw_data_df['tenant_id'].unique():
-            # tenant specific data
-            tenant_data_df = raw_data_df[raw_data_df['tenant_id'] == tenant_id]
-            # TODO: potential extra data engineering at the tenant level
-            # now save tenant training data to file into training data folder
-            tenant_metadata_file_name = "tenant_" + str(tenant_id) + '.csv'
-            with open(os.path.join(args.training_data_folder,tenant_metadata_file_name),'w') as tenant_metadata_file:
-                tenant_data_df.to_csv(tenant_metadata_file,index=False)
-            print(f"{tenant_metadata_file_name} generated.")
-            # add file to MLTable metadata file descriptor for training step
-            MLTable_metadata_file.write(f"  - file: ./{tenant_metadata_file_name}\n")
+        with open(os.path.join(args.evaluation_data_folder,'MLTable'),'w') as MLTable_Evaluation_metadata_file:
+            
+            MLTable_metadata_file.write("paths:\n")
+            MLTable_Evaluation_metadata_file.write("paths:\n")
+
+
+            
+            for client_id in raw_data_df['ClientID'].unique()[:2]:
+                # tenant specific data
+                customer_data_df = raw_data_df[raw_data_df['ClientID'] == client_id]
+
+
+                # TODO: potential extra data engineering at the tenant level
+
+                # extract a evaluation dataset for final evaluation
+                #finalEvalDataset=customer_data_df.iloc[0:-]
+                train, finalEvalDataset = train_test_split(customer_data_df, test_size=0.2,shuffle=False)
+
+            
+
+
+
+                # now save tenant training data to file into training data folder
+                client_metadata_file_name = "tenant_" + str(client_id) + '.csv'
+                with open(os.path.join(args.training_data_folder,client_metadata_file_name),'w') as tenant_metadata_file:
+                    train.to_csv(tenant_metadata_file,index=False)
+                
+                print(f"{client_metadata_file_name} generated.")
+                # add file to MLTable metadata file descriptor for training step
+                MLTable_metadata_file.write(f"  - file: ./{client_metadata_file_name}\n")
+
+
+                # now save tenant evaluation data to file into evaluation data folder
+                with open(os.path.join(args.evaluation_data_folder,client_metadata_file_name),'w') as tenant_evaluation_metadata_file:
+                    finalEvalDataset.to_csv(tenant_evaluation_metadata_file,index=False)
+
+
+                print(f"Evaluation: {client_metadata_file_name} generated.")
+                MLTable_Evaluation_metadata_file.write(f"  - file: ./{client_metadata_file_name}\n")
 
     # alternate MLTable metadata file content (not safe if your destination folder isn't guaranteed to be empty when this runs)
     # paths:
@@ -31,6 +79,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--raw_data_file', type=str)
     parser.add_argument('--training_data_folder', type=str)
+    parser.add_argument('--evaluation_data_folder', type=str)
     args, unknown_args = parser.parse_known_args()
     print(f"raw_data_file: {args.raw_data_file}")
     print(f"training_data_folder: {args.training_data_folder}")
--- training/training.py
+++ training/training.py
@@ -1,6 +1,14 @@
 import argparse,os,time
+from pathlib import Path
 import pandas as pd
 from azureml_user.parallel_run import EntryScript
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from azureml.core import Model
+import mlflow
+import mlflow.sklearn
+from azureml.core import Run, Dataset,Datastore, Workspace
+import joblib
 
 # init() called once per process
 def init():
@@ -9,12 +17,18 @@ def init():
     parser = argparse.ArgumentParser()
     parser.add_argument("--param_1", type=str)
     parser.add_argument("--predictions_data_folder", type=str)
+    parser.add_argument("--model_train_runID", type=str)
+    parser.add_argument("--model_output_folder", type=str)
     args, unknown = parser.parse_known_args()
     global param_1
     global predictions_data_folder
+    global model_train_runID
     param_1 = args.param_1
     predictions_data_folder = args.predictions_data_folder
-
+    model_train_runID=args.model_train_runID
+    
+    global model_output_folder
+    model_output_folder = args.model_output_folder
     # retrieve environment variables
     global env_var_1
     env_var_1 = os.environ['env_var_1']
@@ -23,24 +37,265 @@ def init():
     global logger
     logger = EntryScript().logger
     logger.info(f"train init(), param_1:{param_1}, env_var_1:{env_var_1}")
+
+    run = Run.get_context()
+    ws = run.experiment.workspace
+    global run_id
+    run_id = run.id
+    #with open(os.path.join(model_train_runID),'w') as modelIDFile:
+    # log to global results object
+    #    modelIDFile.write(run_id)
+    
+    print("run_id is {}".format(run_id))
+
+    with open(os.path.join(model_train_runID,"model_train_runID"),'w') as model_train_runIDFile:
+        print("At training, run id is: ", run_id)
+        model_train_runIDFile.write(run_id)
+
+    """    with open((Path(model_train_runID) / "model_train_runID.txt"), 'w') as f:
+               
+        f.write(run_id)
+        print("At training, run id is: ", run_id)
+    """
    
 # run() called as many times as needed to process all files in the parallel job input
 def run(mini_batch):
     
     results = []
     logger.info(f"train run({mini_batch})")
-    for tenant_file_path in mini_batch:
-        tenant_basename = os.path.basename(tenant_file_path)
+    for client_file_path in mini_batch:
+        client_basename = os.path.basename(client_file_path)
         # process the tenant training data file to train a model + infer predictions for the final evaluation step
-        with open(tenant_file_path,'r') as tenant_file:
-            tenant_df = pd.read_csv(tenant_file)
-        logger.info(f"train processing({tenant_basename} => {tenant_df}) with param_1:{param_1}, env_var_1:{env_var_1}")
+        with open(client_file_path,'r') as tenant_file:
+            client_df = pd.read_csv(tenant_file)
+        logger.info(f"train processing({client_basename} => {client_df}) with param_1:{param_1}, env_var_1:{env_var_1}")
         # TODO: replace this part with your model training
-        time.sleep(1) 
-        # TODO: write the output of your models predictions for further performance evaluation of your strategy
-        with open(os.path.join(predictions_data_folder,tenant_basename),'w') as prediction_file:
-            tenant_df.to_csv(prediction_file,index=False)
-        # log to global results object
-        results.append(f"{tenant_basename} processed")
+        #time.sleep(1) 
 
+
+        """        y=client_df["Due_Paid_weekDelta"]
+        x = client_df.drop(columns="Due_Paid_weekDelta")
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, shuffle=False)
+
+        model = LinearRegression().fit(x_train.drop(columns=["DueDate","PaidDate","RaisedDate"]), y_train)"""
+        
+
+        X, y, sample_weights = prepare_data(client_df) 
+        split_ratio = 0.25
+        (X_train, y_train, sample_weights_train), (X_valid, y_valid, sample_weights_valid) = split_dataset(X, y, sample_weights, split_ratio, should_stratify=False)
+        model = train_model(X_train.drop(columns=["DueDate","PaidDate","RaisedDate"]), y_train, sample_weights_train)
+
+
+
+
+
+        mlflow.sklearn.log_model(model, "model_{}".format(client_basename.split(".")[0]))
+
+        
+        #joblib.dump(model, os.path.join(model_output_folder,"model_{}.joblib".format(client_basename.split(".")[0])))
+
+        #myRunID= mlflow.active_run().info.run_id
+        #print("this is myRunID: ", myRunID)
+        
+        #test_result = model.predict(x_test.drop(columns=["DueDate","PaidDate","RaisedDate"]))
+        
+        test_result = model.predict(X_valid.drop(columns=["DueDate","PaidDate","RaisedDate"]))
+
+        # TODO: write the output of your models predictions for further performance evaluation of your strategy
+        outputToReturn=X_valid.assign(y_test=y_valid)
+        
+        outputToReturn["Label"]=test_result
+        with open(os.path.join(predictions_data_folder,client_basename),'w') as prediction_file:
+            outputToReturn.to_csv(prediction_file,index=False)
+
+
+        
+        results.append(f"{client_basename} processed")
+        
+    results.append(f"run_Id is {run_id}")
     return results
+
+ 
+ 
+def setup_instrumentation():
+    import logging
+    import sys
+
+    from azureml.core import Run
+    from azureml.telemetry import INSTRUMENTATION_KEY, get_telemetry_log_handler
+    from azureml.telemetry._telemetry_formatter import ExceptionFormatter
+
+    logger = logging.getLogger("azureml.training.tabular")
+
+    try:
+        logger.setLevel(logging.INFO)
+
+        # Add logging to STDOUT
+        stdout_handler = logging.StreamHandler(sys.stdout)
+        logger.addHandler(stdout_handler)
+
+        # Add telemetry logging with formatter to strip identifying info
+        telemetry_handler = get_telemetry_log_handler(
+            instrumentation_key=INSTRUMENTATION_KEY, component_name="azureml.training.tabular"
+        )
+        telemetry_handler.setFormatter(ExceptionFormatter())
+        logger.addHandler(telemetry_handler)
+
+        # Attach run IDs to logging info for correlation if running inside AzureML
+        try:
+            run = Run.get_context()
+            parent_run = run.parent
+            return logging.LoggerAdapter(logger, extra={
+                "properties": {
+                    "codegen_run_id": run.id,
+                    "parent_run_id": parent_run.id
+                }
+            })
+        except Exception:
+            pass
+    except Exception:
+        pass
+
+    return logger
+
+
+def get_mapper_b86fd6(column_names):
+
+    from azureml.training.tabular.featurization.categorical.cat_imputer import CatImputer
+    from azureml.training.tabular.featurization.datetime.datetime_transformer import DateTimeFeaturesTransformer
+    from azureml.training.tabular.featurization.text.stringcast_transformer import StringCastTransformer
+    from sklearn_pandas.dataframe_mapper import DataFrameMapper
+    from sklearn_pandas.features_generator import gen_features
+    
+    definition = gen_features(
+        columns=column_names,
+        classes=[
+            {
+                'class': CatImputer,
+                'copy': True,
+            },
+            {
+                'class': StringCastTransformer,
+            },
+            {
+                'class': DateTimeFeaturesTransformer,
+            },
+        ]
+    )
+    mapper = DataFrameMapper(features=definition, input_df=True, sparse=True)
+    
+    return mapper
+    
+
+def generate_data_transformation_config():
+    from sklearn.pipeline import FeatureUnion
+    
+    column_group_1 = ['RaisedDate', 'DueDate']
+    
+    mapper = get_mapper_b86fd6(column_group_1)
+    return mapper
+    
+    
+def generate_preprocessor_config():
+    from sklearn.preprocessing import StandardScaler
+    
+    preproc = StandardScaler(
+        copy=True,
+        with_mean=True,
+        with_std=True
+    )
+    
+    return preproc
+    
+    
+def generate_algorithm_config():
+
+    #from sklearn.ensemble import ExtraTreesRegressor
+    from sklearn.linear_model import LinearRegression
+    """  algorithm = ExtraTreesRegressor(
+        bootstrap=False,
+        ccp_alpha=0.0,
+        criterion='mse',
+        max_depth=None,
+        max_features=0.5,
+        max_leaf_nodes=None,
+        max_samples=None,
+        min_impurity_decrease=0.0,
+        min_impurity_split=None,
+        min_samples_leaf=0.004196633747563344,
+        min_samples_split=0.000753222139758624,
+        min_weight_fraction_leaf=0.0,
+        n_estimators=25,
+        n_jobs=-1,
+        oob_score=False,
+        random_state=None,
+        verbose=0,
+        warm_start=False
+    )"""
+
+
+    algorithm = LinearRegression()
+
+
+    
+    return algorithm
+
+
+
+#   ('featurization', generate_data_transformation_config()),
+def build_model_pipeline():
+    from sklearn.pipeline import Pipeline
+    
+    logger.info("Running build_model_pipeline")
+    pipeline = Pipeline(
+        steps=[
+            ('preproc', generate_preprocessor_config()),
+            ('model', generate_algorithm_config()),
+        ]
+    )
+    
+    return pipeline
+
+def split_dataset(X, y, weights, split_ratio, should_stratify):
+    from sklearn.model_selection import train_test_split
+
+    random_state = 42
+    if should_stratify:
+        stratify = y
+    else:
+        stratify = None
+
+    if weights is not None:
+        X_train, X_test, y_train, y_test, weights_train, weights_test = train_test_split(
+            X, y, weights, stratify=stratify, test_size=split_ratio, random_state=random_state
+        )
+    else:
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, stratify=stratify, test_size=split_ratio, random_state=random_state
+        )
+        weights_train, weights_test = None, None
+
+    return (X_train, y_train, weights_train), (X_test, y_test, weights_test)
+
+def prepare_data(dataframe):
+    from azureml.training.tabular.preprocessing import data_cleaning
+    
+    logger.info("Running prepare_data")
+    label_column_name = 'Due_Paid_weekDelta'
+    
+    # extract the features, target and sample weight arrays
+    y = dataframe[label_column_name].values
+    X = dataframe.drop([label_column_name], axis=1)
+    sample_weights = None
+    X, y, sample_weights = data_cleaning._remove_nan_rows_in_X_y(X, y, sample_weights,
+     is_timeseries=False, target_column=label_column_name)
+    
+    return X, y, sample_weights
+
+
+def train_model(X, y, sample_weights=None, transformer=None):
+    logger.info("Running train_model")
+    model_pipeline = build_model_pipeline()
+    
+    model = model_pipeline.fit(X, y)
+    return model
